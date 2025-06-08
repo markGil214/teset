2efb7dbec5cfda6b743024f0a416cfd878ae5175
markGil214 <22as1664_ms@psu.edu.ph>
2025-06-08 09:47:22 +0800

    refresh

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [".ngrok-free.app"],
+    allowedHosts: [".ngrok-free.app", ".share.zrok.io"],
   },
 });
