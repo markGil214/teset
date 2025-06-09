b29448d110adfb66583fca886ead63bcae6b0eb1
markGil214 <22as1664_ms@psu.edu.ph>
2025-06-09 10:20:07 +0800

    uploading new attempt 3

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,16 @@ export default defineConfig({
   server: {
     allowedHosts: [".ngrok-free.app", ".share.zrok.io"],
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        ARScannerPage: 'public/ARScannerPage.html',
+        BrainViewer: 'public/BrainViewer.html',
+        KidneyViewer: 'public/KidneyViewer.html',
+        LungsViewer: 'public/LungsViewer.html',
+        SlicedHeartViewer: 'public/SlicedHeartViewer.html'
+      }
+    }
+  }
 });
